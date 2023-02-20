50fb846474613ccac67cbbe91faca446c5013dd1
Salam Dalloul <salam@leannode.com>
2023-02-20 23:14:58 +0100

    add nav tabs on top of carousel + reflect nav click on showing slides

--- js/osb.js
+++ js/osb.js
@@ -1,40 +1,12 @@
-/*!
- * Start Bootstrap - Agnecy Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $("#"+$anchor.attr('href').split('#')[1]).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
 
-$(function() {
-    $('a.anchor-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        var elementsWithSameClass = $('.'+($anchor.attr('href').split('#'))[1]) //get the class name from href value
-        // out of the two elements find the visible one, get its top value
-        var scrollTop = $(elementsWithSameClass[0]).is(':visible') ? $(elementsWithSameClass[0]).offset().top : $(elementsWithSameClass[1]).offset().top
-        $('html, body').stop().animate({
-            scrollTop: scrollTop - 130
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
-
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
+$('a[data-bs-toggle="tab"]').on('click', (e) => {
+    console.log(e.target.id)
+    const activeTab = e.target.id
+    const activeElement = document.querySelector('.carousel-item.active')
+    activeElement.classList.remove('active')
+    console.log(`${activeTab}-carousel-item`)
+    const newActiveElement = document.getElementById(`${activeTab}-carousel-item`)
+    newActiveElement.classList.add('active')
+    console.log(newActiveElement)
 })
-
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
-
