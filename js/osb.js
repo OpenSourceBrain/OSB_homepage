af062e346c8a9b1239dd4247c62cbff5025ed735
Salam Dalloul <salam@leannode.com>
2023-02-21 12:40:07 +0100

    remove console.log

--- js/osb.js
+++ js/osb.js
@@ -11,19 +11,11 @@ $('a[data-bs-toggle="tab"]').on('click', (e) => {
 $('#features-carousel').on('slid.bs.carousel', function onSlide (ev) {
     const id = ev.relatedTarget.id;
     const activeTab = document.querySelector('.nav-link.active')
-    console.log(activeTab)
     const newActiveTabId = id.replace('-carousel-item', '')
     activeTab.classList.remove('active')
     activeTab.setAttribute('aria-selected', 'false')
-    console.log(newActiveTabId)
     const newActiveTab = document.getElementById(newActiveTabId)
 
     newActiveTab.classList.add('active')
     newActiveTab.setAttribute('aria-selected', 'true')
-
-    console.log(newActiveTabId)
-    // activeElement.classList.remove('active')
-    // const newActiveElementId = id.replace('-carousel-item', '')
-    // console.log(newActiveElement)
-    // newActiveElement.classList.add('active')
 })
