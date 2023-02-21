2c0463044194a2c8ce3b2fa582b65a7b63493840
Salam Dalloul <salam@leannode.com>
2023-02-21 12:38:02 +0100

    reflect carousel changes to tabs + fix tabs style

--- _site/js/osb.js
+++ _site/js/osb.js
@@ -1,12 +1,29 @@
 
 
 $('a[data-bs-toggle="tab"]').on('click', (e) => {
-    console.log(e.target.id)
     const activeTab = e.target.id
     const activeElement = document.querySelector('.carousel-item.active')
     activeElement.classList.remove('active')
-    console.log(`${activeTab}-carousel-item`)
     const newActiveElement = document.getElementById(`${activeTab}-carousel-item`)
     newActiveElement.classList.add('active')
-    console.log(newActiveElement)
+})
+
+$('#features-carousel').on('slid.bs.carousel', function onSlide (ev) {
+    const id = ev.relatedTarget.id;
+    const activeTab = document.querySelector('.nav-link.active')
+    console.log(activeTab)
+    const newActiveTabId = id.replace('-carousel-item', '')
+    activeTab.classList.remove('active')
+    activeTab.setAttribute('aria-selected', 'false')
+    console.log(newActiveTabId)
+    const newActiveTab = document.getElementById(newActiveTabId)
+
+    newActiveTab.classList.add('active')
+    newActiveTab.setAttribute('aria-selected', 'true')
+
+    console.log(newActiveTabId)
+    // activeElement.classList.remove('active')
+    // const newActiveElementId = id.replace('-carousel-item', '')
+    // console.log(newActiveElement)
+    // newActiveElement.classList.add('active')
 })
